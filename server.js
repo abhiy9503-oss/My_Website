7e43f9d64120ea389c2f01329fa6e5db0fc6b2a4
abhiy9503-oss <abhiy9503@gmail.com>
2026-04-03 22:25:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// VERY IMPORTANT: serve static files
 app.use(express.static(path.join(__dirname)));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
